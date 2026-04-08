bf597a7cb07fc10901294655b4aff84289e1104e
homePC <smart@KIIWORKSTATION>
2026-04-08 19:50:10 -0400

    Migrations for shipment change

---
+++ ImperialIMS/ImperialIMS/Migrations/20260408234955_UpdatedShipments.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ImperialIMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedShipments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EstimatedDeliveryDate",
+                table: "Shipmetns",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EstimatedDeliveryDate",
+                table: "Shipmetns");
+        }
+    }
+}
